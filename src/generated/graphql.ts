edda7f53d22664cb63f2d77a32ae585b258fb562
Hosmel Quintana <hosmelq@gmail.com>
2020-08-22 09:13:22 -0600

    add generated types

---
+++ src/generated/graphql.ts
@@ -0,0 +1,57 @@
+/* eslint-disable */
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** An RFC 3986 and RFC 3987 compliant URI string. */
+  URI: string;
+  /** An ISO-8601 encoded UTC date time string. Example value: `2019-07-03T20:47:55Z` */
+  DateTime: string;
+  /** A [RFC 5321](https://tools.ietf.org/html/rfc5321) compliant email. */
+  Email: string;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** The available directions for ordering a list of records. */
+export enum SortOrder {
+  /** Sort records in ascending order. */
+  ASC = 'ASC',
+  /** Sort records in descending order. */
+  DESC = 'DESC'
+}
+
+/** Allows ordering a list of records. */
+export type OrderByClause = {
+  /** The column that is used for ordering. */
+  column: Scalars['String'];
+  /** The direction that is used for ordering. */
+  order: SortOrder;
+};
+
+
+/** Specify if you want to include or exclude trashed results from a query. */
+export enum Trashed {
+  /** Only return trashed results. */
+  ONLY = 'ONLY',
+  /** Return both trashed and non-trashed results. */
+  WITH = 'WITH',
+  /** Only return non-trashed results. */
+  WITHOUT = 'WITHOUT'
+}
