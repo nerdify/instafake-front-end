6add8383ad8334e96b36574eed4f338140f1681b
sald19 <carl.sald19@gmail.com>
2020-08-24 08:10:16 -0600

    add post to timeline and create post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Avatar, Box, Flex, Text, Heading} from '@chakra-ui/core'
+import {faEllipsisH as fasEllipsisH} from '@fortawesome/pro-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+export function Post() {
+  return (
+    <Box bg="white" border="1px solid" borderColor="gray.200" borderRadius="md">
+      <Flex
+        justify="space-between"
+        align="center"
+        py={2}
+        px={4}
+        borderBottom="1px solid"
+        borderColor="gray.200"
+      >
+        <Flex align="center">
+          <Box
+            border="1px solid"
+            borderColor="gray.200"
+            borderRadius="full"
+            p="2px"
+          >
+            <Avatar
+              boxShadow="sm"
+              size="sm"
+              src="https://images.unsplash.com/photo-1577565177023-d0f29c354b69?fit=crop&h=64&w=64&q=80"
+            />
+          </Box>
+          <Heading size="xs" ml={2}>
+            verge
+          </Heading>
+        </Flex>
+        <FontAwesomeIcon icon={fasEllipsisH}></FontAwesomeIcon>
+      </Flex>
+      <Flex>
+        <img src="https://source.unsplash.com/random/1024x600" alt="test" />
+      </Flex>
+    </Box>
+  )
+}
--- src/pages/Timeline/Timeline.tsx
+++ src/pages/Timeline/Timeline.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import {StoriesCard} from './components/StoriesCard'
+import {Post} from '../../components/Post'
+import {Stack} from '@chakra-ui/core'
 
 export function Timeline() {
   return (
     <div>
       <StoriesCard />
+      <Stack spacing={4} pt={8}>
+        {Array.from(Array(10).keys()).map((item) => (
+          <Post key={item} />
+        ))}
+      </Stack>
     </div>
   )
 }
