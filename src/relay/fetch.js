9093923a77d7f96b2e2bcd15b431a543bd9be1b3
Hosmel Quintana <hosmelq@gmail.com>
2020-09-12 10:59:39 -0600

    add REACT_APP_GRAPHQL_TOKEN env

--- src/relay/fetch.js
+++ src/relay/fetch.js
@@ -9,6 +9,7 @@ export default async function _fetch(
   const body = getRequestBody(operation, variables, uploadables)
   const headers = {
     ...getHeaders(uploadables),
+    Authorization: `Bearer ${process.env.REACT_APP_GRAPHQL_TOKEN}`,
   }
   const response = await fetch(process.env.REACT_APP_GRAPHQL_ENDPOINT, {
     body,
